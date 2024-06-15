1db8ba089c83834af9926e588ab0205d36ef3f42
Jaya Sai Gorre <123870905+jayasai-gorre@users.noreply.github.com>
2024-06-15 12:33:32 +0530

    updating linkedin and github profiles

--- src/sections/Hero/Hero.jsx
+++ src/sections/Hero/Hero.jsx
@@ -42,10 +42,10 @@ const Hero = () => {
             <a href="https://twitter.com" target="_blank">
                 <img src={twitterIcon} alt="Twitter icon" />
             </a>
-            <a href="https://github.com" target="_blank">
+            <a href="https://github.com/jayasai-gorre" target="_blank">
                 <img src={githubIcon} alt="Github icon" />
             </a>
-            <a href="https://linkedin.com" target="_blank">
+            <a href="https://linkedin.com/in/jayasai-gorre" target="_blank">
                 <img src={linkedinIcon} alt="Linkedin icon" />
             </a>
         </span>
